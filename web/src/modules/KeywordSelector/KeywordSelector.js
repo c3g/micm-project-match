c464ce3d04b62d47e6def34b9217512a77644e54
pranavtharoor <pranav.tharoor@gmail.com>
2019-07-11 05:29:29 +0530

    Styled keyword selector

--- web/src/modules/KeywordSelector/KeywordSelector.js
+++ web/src/modules/KeywordSelector/KeywordSelector.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import RoundedInputField from 'Src/modules/RoundedInputField';
+import './keywordSelector.scss';
 
 class KeywordSelector extends Component {
   static propTypes = {
@@ -18,35 +19,50 @@ class KeywordSelector extends Component {
 
   onChange = e => {
     this.setState({ term: e.target.value });
-    this.props.searchKeyword(e.target.value);
+    if (e.target.value !== '') this.props.searchKeyword(e.target.value);
   };
 
   onClick = () => this.props.createKeyword(this.state.term);
 
   render() {
     return (
-      <div>
+      <div className="keyword-selector">
+        Selected keywords:
+        <div className="selected">
+          {this.props.selected.length > 0 ? (
+            this.props.selected.map((keyword, i) => (
+              <div
+                onClick={() => this.props.deselectKeyword(keyword)}
+                key={`selected_keyword_${i}`}
+              >
+                {keyword.text}
+              </div>
+            ))
+          ) : (
+            <span className="message">None</span>
+          )}
+        </div>
         <RoundedInputField
+          onKeyDown={e => e.keyCode === 13 && e.preventDefault()}
           onChange={this.onChange}
           placeholder="Search for keywords to add..."
         />
-        <div onClick={this.onClick}>Add</div>
-        {this.props.selected.map((keyword, i) => (
-          <div
-            onClick={() => this.props.deselectKeyword(keyword)}
-            key={`selected_keyword_${i}`}
-          >
-            {keyword.text}
-          </div>
-        ))}
-        {this.props.keywords.map((keyword, i) => (
-          <div
-            onClick={() => this.props.selectKeyword(keyword)}
-            key={`keyword_${i}`}
-          >
-            {keyword.text}
-          </div>
-        ))}
+        <div className="add">
+          {this.state.term !== '' && !this.props.keywords.length && (
+            <div onClick={this.onClick}>Add</div>
+          )}
+        </div>
+        <div className="suggestions">
+          {this.state.term !== '' &&
+            this.props.keywords.map((keyword, i) => (
+              <div
+                onClick={() => this.props.selectKeyword(keyword)}
+                key={`keyword_${i}`}
+              >
+                {keyword.text}
+              </div>
+            ))}
+        </div>
       </div>
     );
   }
