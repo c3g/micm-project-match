97ab068bc5fe33de93020f07e3232eccdf374105
Rom Grk <romgrk.cc@gmail.com>
2020-01-03 14:45:50 -0500

    api: fix file utils timeout issues

--- api/src/utils/file.js
+++ api/src/utils/file.js
@@ -1,8 +1,25 @@
 import path from 'path';
+import exists from 'path-exists';
 import fs, { promises as fsPromises } from 'fs';
 
 const basePath = process.env.STORAGE_PATH
-const makeParentDir = filepath => fsPromises.mkdir(path.dirname(filepath), { recursive: true })
+const makeParentDir = async filepath => {
+  const dirname = path.dirname(filepath)
+  const dirExists = await exists(dirname)
+  if (!dirExists)
+    await fsPromises.mkdir(dirname, { recursive: true })
+}
+
+const writeFile = (filepath, content) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filepath, content, (err) => {
+      if (err)
+        reject(err)
+      else
+        resolve()
+    })
+  })
+}
 
 const MB = 2 ** 20
 
@@ -14,9 +31,7 @@ export const checkSize = (file, maxMB) => {
 
 export const upload = (location, mimeType, content) =>
   makeParentDir(path.join(basePath, location))
-  .then(() =>
-    fsPromises.writeFile(path.join(basePath, location), content)
-  );
+    .then(() => writeFile(path.join(basePath, location), content))
 
 export const deleteObject = location =>
   fsPromises.unlink(path.join(basePath, location));
