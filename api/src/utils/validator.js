705ba55410f6d9370944a814329e7f09560e8f30
Rom Grk <romgrk.cc@gmail.com>
2020-01-17 16:48:57 -0500

    api: validator: always show details

--- api/src/utils/validator.js
+++ api/src/utils/validator.js
@@ -5,18 +5,13 @@ const validator = schema => (req, res, next) => {
   if (Object.keys(req.query).length) data.query = req.query;
   if (Object.keys(req.params).length) data.params = req.params;
   if (Object.keys(req.body).length) data.body = req.body;
+
   Joi.validate(data, schema, { abortEarly: false }, (err, value) => {
     if (err)
       return res.status(422).json({
         success: false,
-        message:
-          process.env.NODE_ENV === 'production'
-            ? 'Invalid input'
-            : err.details
-                .map(({ message, path }) => {
-                  return `${message}${path ? ` (${path})` : ''}`;
-                })
-                .toString()
+        message: err.details
+          .map(({ message, path }) => `${message}${path ? ` (${path})` : ''}`).toString()
       });
     req.payload = value;
     next();
