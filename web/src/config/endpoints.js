09fd35c11682ad4cb9fad5ddfe0fd94ce48c5f6d
Rom Grk <romgrk.cc@gmail.com>
2020-01-20 14:51:01 -0500

    web: fix endpoints config

--- web/src/config/endpoints.js
+++ web/src/config/endpoints.js
@@ -1,8 +1,8 @@
-export const url = 'http://localhost:3000';
+export const url = process.env.PUBLIC_URL;
 export const prefix = '/api';
 export const api = `${prefix}`;
-export const facebookLogin = `${url}${prefix}/auth/facebook/`;
-export const googleLogin = `${url}${prefix}/auth/google/`;
+export const facebookLogin = `${url}/${prefix}/auth/facebook/`;
+export const googleLogin = `${url}/${prefix}/auth/google/`;
 
 export default {
   api,
