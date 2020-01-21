076a5ab49f4eee92f7ff49bed2804ec611c076e6
Rom Grk <romgrk.cc@gmail.com>
2020-01-21 12:57:49 -0500

    web: improve UserList interface

--- web/src/modules/UserList/UserList.js
+++ web/src/modules/UserList/UserList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Loader from 'Src/modules/Loader';
+import Icon from 'Src/modules/Icon';
 import * as k from 'Src/constants/values';
 import './userList.scss';
 
@@ -37,45 +38,56 @@ class UserList extends Component {
 
   render() {
     const {
+      isLoading,
       currentUser,
       users,
       makeAdmin,
       makeProfessor,
       makeStudent
     } = this.props;
+
     return (
-      <div className="user-list">
-        {this.props.isLoading ? (
-          <Loader />
-        ) : (
-          users.map((user, i) =>
-            user.id === currentUser.id ? null : (
-              <div className="user" key={`user_${i}`}>
-                <div>
-                  <Link to={`/user/${user.id}`}>
-                    {user.firstName} {user.lastName}
-                  </Link>
-                  <div className="user-type">{user.type}</div>
-                </div>
-                <div className="button-group">
-                  {user.type !== k.ADMIN && (
-                    <button onClick={() => makeAdmin(user.id)}>
-                      Make Admin
-                    </button>
-                  )}
-                  {user.type !== k.PROFESSOR && (
-                    <button onClick={() => makeProfessor(user.id)}>
-                      Make Professor
-                    </button>
-                  )}
-                  {user.type !== k.STUDENT && (
-                    <button onClick={() => makeStudent(user.id)}>
-                      Make Student
-                    </button>
-                  )}
-                </div>
+      <div className="UserList">
+        {isLoading && <Loader />}
+
+        <div className="UserList__header flex-row">
+          <div className="UserList__name">Name</div>
+          <div className="UserList__type">Type</div>
+          <div className="UserList__approved">Approved</div>
+          <div className="flex-fill" />
+        </div>
+        {users.map((user, i) =>
+          user.id === currentUser.id ? null : (
+            <div
+              key={`user_${i}`}
+              className="UserList__item flex-row flex-align-center"
+            >
+              <Link className="UserList__name" to={`/user/${user.id}`}>
+                {user.firstName} {user.lastName}
+              </Link>
+              <div className="UserList__type">{user.type}</div>
+              <div className="UserList__approved">
+                {user.approved ? 'Yes' : 'No'}
               </div>
-            )
+              <div className="flex-fill" />
+              <div className="button-group">
+                {user.type !== k.PROFESSOR && (
+                  <button onClick={() => makeProfessor(user.id)}>
+                    Make Professor
+                  </button>
+                )}
+                {user.type !== k.STUDENT && (
+                  <button onClick={() => makeStudent(user.id)}>
+                    Make Student
+                  </button>
+                )}
+                {user.type !== k.ADMIN && (
+                  <button onClick={() => makeAdmin(user.id)}>
+                    Make Admin <Icon name="exclamation-triangle" />
+                  </button>
+                )}
+              </div>
+            </div>
           )
         )}
       </div>
