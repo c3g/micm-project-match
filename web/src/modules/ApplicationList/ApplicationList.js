f26c8de5decb617e28b34f9ed5d5d13281971bef
Rom Grk <romgrk.cc@gmail.com>
2020-02-18 14:51:02 -0500

    web: make applications exportable

--- web/src/modules/ApplicationList/ApplicationList.js
+++ web/src/modules/ApplicationList/ApplicationList.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ApplicationPropType from 'Src/propTypes/Application';
 import ApplicationListItem from 'Src/modules/ApplicationListItem';
+import RoundedButton from 'Src/modules/RoundedButton';
 import Loader from 'Src/modules/Loader';
+import csvStringify from 'csv-stringify';
+import { saveAs } from 'file-saver';
 
 class ApplicationList extends Component {
   static propTypes = {
@@ -17,19 +20,68 @@ class ApplicationList extends Component {
   }
 
   render() {
+    const { applications } = this.props;
+
     return (
       <div className="application-list">
         {this.props.isLoading ? (
           <Loader />
         ) : (
-          this.props.applications.map((application, i) => (
-            <div key={`application_${i}`}>
-              <ApplicationListItem data={application} />
+          <React.Fragment>
+            <div className="flex-row">
+              <div className="flex-fill" />
+              <RoundedButton onClick={() => exportAsCSV(applications)}>
+                Export as CSV
+              </RoundedButton>
             </div>
-          ))
+            {
+              applications.map((application, i) => (
+                <div key={`application_${i}`}>
+                  <ApplicationListItem data={application} />
+                </div>
+              ))
+            }
+          </React.Fragment>
         )}
       </div>
     );
   }
 }
+
+function exportAsCSV(applications) {
+  const headers = [
+    'Name',
+    'Study Program',
+    'Study Year',
+    'Graduation Year',
+    'University',
+    'Other internships?',
+  ];
+
+  const options = {
+    header: true,
+    columns: headers
+  };
+
+  const records = applications.map(({ application, user }) => [
+    `${user.firstName} ${user.lastName}`,
+    application.studyProgram,
+    application.studyYear,
+    application.graduationYear,
+    application.university,
+    application.otherInternships ?
+      `Yes: ${application.otherInternshipsDetails}` : 'No',
+  ]);
+
+  csvStringify(records, options, (err, content) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
+    saveAs(blob, 'application-list.csv');
+  });
+}
+
 export default ApplicationList;
