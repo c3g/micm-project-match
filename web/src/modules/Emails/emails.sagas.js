418b1381fd02bfe4ff2f8655370bc6bf6e716d47
Rom Grk <romgrk.cc@gmail.com>
2020-06-01 18:42:10 -0400

    lint

--- web/src/modules/Emails/emails.sagas.js
+++ web/src/modules/Emails/emails.sagas.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest, delay } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { push } from 'connected-react-router';
 import { request, action } from 'Src/utils';
 import { EMAIL, SNACKBAR } from 'Src/constants/actionTypes';
